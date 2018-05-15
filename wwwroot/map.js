16f2853083e569f98537bbf5346b1b7f1e796424
Unknown <jasonk1234567890@gmail.com>
2018-05-15 13:05:38 -0400

    all layers added

--- wwwroot/map.js
+++ wwwroot/map.js
@@ -4,7 +4,7 @@ function getAddr(id){
         
         l=document.getElementById("location").value=id;
         localStorage.setItem("l", l);
-        location.href ="http://localhost:54954/evac-map";
+        location.href ="http://localhost:5000/evac-map";
         //l=document.getElementById("location").value=id;
 }
 function getIntervalPlease(){
@@ -68,7 +68,10 @@ var barrierSymbol;
         var featureLayer = new FeatureLayer("https://services1.arcgis.com/jjVcwHv9AQEq3DH3/arcgis/rest/services/Evacuation_Zones/FeatureServer/0");
         //map.addLayer(featureLayer);
         var flood10 = new FeatureLayer("https://services1.arcgis.com/X3lKekbdaBmNjCHu/arcgis/rest/services/flood10_danger/FeatureServer/0?token=OVDNTKAJhXH8QDp7W78e2wMkAO7EBiBdmVt1typYCyBCdAXFrRPGAwhqQNmRg_144Hx9Wei8n6bozFhwnDMLQRwKG5S2kgHG_MEYkEkM_pnlZkneQYK2oDyk3eiEL37en1xA5-XYjjTbHE5DNQXZHghYUbvHBdkFnavbdY2nJxVtWa89HFhSWW7JatmSN-N7kbYrFPpNAoD5rbME7jrQImqyrWlOrooPebokZI0nmx0eCS6RdEYN463QEdJ8FxnS");
-       
+        var flood25 = new FeatureLayer("https://services1.arcgis.com/X3lKekbdaBmNjCHu/arcgis/rest/services/flood25_danger/FeatureServer/0?token=SdqFMfnqrqk-ANoWaHM_xbEhNvgVEZmjS6ezHTsqsZp0mR8MYDaLV4P_k-LCnZdPAPjNA37ppH4TX55Lihjz6a6PGnx8-eBpMk9x_NPeRVr90Ci5f68onzZzxVgbR8ZncwaC00X7FRh6ir6nyyeEnCk6hGXUS7s8WwX0WYS8P2VXOV9M7W8Z0RVAwewi8S7JnDrpsWKUYkWolJxn9Qmq-GrXdnZGErvJgIAocdaWd9TqRgdoS5GAO7katvWD-cln"); 
+        var flood50 = new FeatureLayer("https://services1.arcgis.com/X3lKekbdaBmNjCHu/arcgis/rest/services/flood50_danger/FeatureServer/0?token=lhsm4h9XvjFAk1-YVKyqiofctj1c1gTv96jKvDWz8kezGM8wN5JkllooSRK1rDMiOeZCCzLsaCKD0-KIl4WakclI_0XULDlrFk7ARM_KF9mo6Qw2KZ7iLadoKBYOI_iB7-ZORYJJt7ZAUCznsaSBQ1fgF-ESGBJKZJcN308dTphqQx4lQDKqlE2jCjH1xB-o1W1tOn9eQY6HQ6FLXbVJ6eu1j-ZSBqvF53BuD0xp44dxSX39P2RqVaYK2qJSqvXW"); 
+        var flood100= new FeatureLayer("https://services1.arcgis.com/X3lKekbdaBmNjCHu/arcgis/rest/services/flood100_danger/FeatureServer/0?token=Z5STpfWrUKEWPHUpnJ-SF8CmowMT2-W40BZ2-tm7AqD8ZVBiz4OphFrD6DvNCBgiWzQSOXyOFTdOcgbyX9v28sk43Pg3DN_suNisT0REFsYIVuoAbq79IjsORpXgewcsTDPXOvc8QvpWOHADfRJcOufzKKbtCxE-VEizWHDpVu0Zj3cTouspAnKoZcGfkPxjVP80jqDvURi-WnWNSnZmQ6IL0HVqa1ngPe5rS2GS1nQHOXOmbiBrHabieOOnpQfl"); 
+        
 
         var d = document.getElementById("dir");
 
@@ -96,6 +99,21 @@ var point = new Point(-75.062644, 38.395349);
         if(interval==10){
             map.addLayer(flood10);
         }
+        else if (interval == 25) { 
+            map.addLayer(flood25); 
+        } 
+        else if (interval == 50) { 
+            map.addLayer(flood50); 
+        } 
+        else if(interval == 100){ 
+            map.addLayer(flood100); 
+        } 
+        else{ 
+            map.addLayer(featureLayer); 
+        } 
+ 
+
+
 
         if(l=="2611 N Philadelphia Ave, Ocean City, MD 21842"){
             directions.addStop(point1, 1);
